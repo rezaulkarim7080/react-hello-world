029885265d5c3c35c702d59b03aa64f71690c16f
rezaulkarim7080 <outsourcecare125@gmail.com>
2022-10-15 15:59:33 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,46 @@ import logo from './logo.svg';
 import './App.css';
 import Book from './components/Book';
 import Person from './components/Person';
+import Product from './components/product';
+
+// function App() {
+//   return (
+//     <div className="App">
+//      <h1>React App JS</h1>
+//      <h1>Book List</h1>
+//      <Book BookName ="Mayabini" writter ="homayon kabir "/>
+     
+//     </div>
+//   );
+// }
+
+
+//// USEING PRODUCT COMPONENT 
 
 function App() {
+const array ={name1:"apple",name2:"tesla"}
+
   return (
     <div className="App">
      <h1>React App JS</h1>
-     <h1>Book List</h1>
-     <Book BookName ="Mayabini" writter ="homayon kabir "/>
+     <Product productName ="Photoshop" productprice="55.99$"/>
+     <Product productName ="Illustrator" productprice="45.99$"/>
+    <Product productName ="	Internet Explorer" productprice="23.99$"/>
+     <Product productName ="Google Chrome" productprice="3.99$"/>
+     <Product productName ="GIMP" productprice="23.99$"/>
+     <Product productName ="LHMelt" productprice="87.99$"/>
+     <Product productName ="ArcGIS" productprice="43.99$"/>
+
+    
+     
      
     </div>
   );
 }
+
+
+
+
+
 
 export default App;
