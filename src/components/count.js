0a9ea54e8b19e1fe3c0bfce5995a63d707bacb43
rezaulkarim7080 <outsourcecare125@gmail.com>
2022-10-16 20:28:26 +0600

    usestate

--- src/components/count.js
+++ src/components/count.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Count() {
+  const [count, setCount] = useState(0);
+  //   const handleIncrease = () => {
+  //     setCount(count + 1);
+  //   };
+
   return (
     <div>
-      <h1>Count : </h1>
+      <h1>Count : {count}</h1>
+      {/* <button onClick={handleIncrease}>Increase</button> */}
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
     </div>
   );
 }
